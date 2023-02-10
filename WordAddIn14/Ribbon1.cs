94611c77a3dc954b83835a0dfad0669c7b103f02
neumannd1991 <neumann.daniel@hm.edu>
2023-02-11 00:01:12 +0100

    Projektdateien hinzufügen.

---
+++ WordAddIn14/Ribbon1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.Office.Tools.Ribbon;
+using Microsoft.Office.Interop.Word;
+
+namespace WordAddIn14
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void Header1_Click(object sender, RibbonControlEventArgs e)
+        {
+            Application wordApp = Globals.ThisAddIn.Application;
+            Template buildingBlock = wordApp.Templates["Header1"];
+            buildingBlock.BuildingBlockEntries.Item(1).Insert(wordApp.Selection.Range, Type.Missing);
+        }
+    }
+}
